2921cbac54ade765f5684b61f0c18f9bc40e16cf
Seongkwon <Seongkwon@localhost>
2017-07-06 18:46:12 +0900

    자바 소개

---
+++ curr01_notepad/src/curr01_notepad/J01_ABC.java
@@ -0,0 +1,51 @@
+package curr01_notepad;
+
+//	수업 내용
+//	1. JAVA 소개
+//		1. 자바소개 - 특징, 장점
+//		2. 컴파일 && 인터프리팅
+//		3. 필요 프로그램 소개 : JDK(Java Development Kit) && JRE(Java Run Environment)
+
+//	2. 프로그램 설치
+//		1. JDK && JRE - www.oracle.com
+
+//	3. 프로그램 작성 및 실행
+//		1. 작성 - 메모장
+//		2. 구성 - 클래스, 메인메소드
+//		3. 컴파일 및 실행
+//			- .java파일 && .class파일
+//		4. 환경변수 설정 후 실행
+
+//	4. 기본 문법
+//		1. print 기본 출력
+//		2. \n
+//		3. println 출력
+//		4. 세미콜론(;)과 프로그램 실행의 순차적 의미
+
+//	1. 주석처리 기호 - //  /**/
+//	2. println - int, double, char, String
+
+
+public class J01_ABC {
+	public static void main(String[] args) {
+		
+		System.out.println("JAVA 프로그램");
+		System.out.println("\n");
+		System.out.println("출력기능 배우기");
+		System.out.println("\n");
+		
+		System.out.println("print : 줄바꿈 없는 출력 기능 \n");
+		
+		System.out.println("println : 줄바꿈이 있는 출력 기능");
+		
+		System.out.println("마지막 출력 내용");
+		
+		System.out.println("문장 출력하기");
+		System.out.println(10);
+		System.out.println(12.34);
+		System.out.println('한');
+		
+//		문제
+//		1. 자기소개하기 - 이름, 나이, 키, 몸무게, 취미 등...
+	}
+}
